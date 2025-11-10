d120b4dafc4de6a4a2481fcdacabe38f4579e536
Hasan Talukder <mahadihasansiam2@gmail.com>
2025-11-10 11:19:20 +0600

    refactor: remove experience and guards modules, controllers, services, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ContactModule } from './contact/contact.module';
-import { ExperienceModule } from './experience/experience.module';
 import { BlogModule } from './blog/blog.module';
 import { SettingsModule } from './settings/settings.module';
 import { BannerModule } from './banner/banner.module';
 import { SocialModule } from './social/social.module';
 import { CareerModule } from './career/career.module';
 import { UploadModule } from './upload/upload.module';
-import { GuardsModule } from './guards/guards.module';
 import { ServicesModule } from './services/services.module';
 import { ClientsModule } from './clients/clients.module';
 import { GalleryModule } from './gallery/gallery.module';
@@ -26,12 +24,10 @@ import { JobsModule } from './jobs/jobs.module';
     AuthModule,
     ProjectsModule,
     ContactModule,
-    ExperienceModule,
     BlogModule,
     SettingsModule,
     BannerModule,
     SocialModule,
-    GuardsModule,
     ServicesModule,
     ClientsModule,
     GalleryModule,
